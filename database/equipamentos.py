b67abb7ba5d9c72ebf4b9e1838076a38f0d21bd1
DevENIND <naorespenind@hotmail.com>
2024-08-30 17:37:03 -0300

    Add files via upload

---
+++ database/equipamentos.py
@@ -0,0 +1,6 @@
+lista_equipamentos=[
+    {"id":1, "Desc":"Hidromentro de 500L", "NumSerie": "DCTM","Prefixo": "H500"},
+    {"id":2, "Desc":"Manometro Spped Teste", "NumSerie": "MAN-003","Prefixo": "975-MAN-003"},
+    {"id":3, "Desc":"Manometro Digital", "NumSerie": "68764","Prefixo": "MAD"},
+    {"id":4, "Desc":"Prensa hidraulica de Conformagem", "NumSerie": "24910","Prefixo": "PHC"}
+    ]
