2f7805ab6d05b875aa4c3bf7d04a271a56b8452c
DevENIND <naorespenind@hotmail.com>
2024-08-30 17:34:29 -0300

    Delete CalibragemEquips directory

--- CalibragemEquips/routes/Ctr_Eqto.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from database.equipamentos import lista_equipamentos
-
-Ctr_Eqto_route = Blueprint('Ctr_Eqto', __name__)
-
-@Ctr_Eqto_route.route('/')
-def lista_eqtos():
-    #Lista os equipamentos
-    return render_template('Ctr_Eqto.html', equipamentos=lista_equipamentos)
